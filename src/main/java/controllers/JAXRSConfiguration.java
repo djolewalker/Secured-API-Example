5d462e46f22cefcf0377b3ca106fbdecb4657807
Dimitrije Zarkovic <dimitrije.zarkovic@material-exchange.com>
2020-03-03 11:43:06 +0100

    Startup class update

--- src/main/java/controllers/JAXRSConfiguration.java
+++ src/main/java/controllers/JAXRSConfiguration.java
@@ -1,9 +1,17 @@
 package controllers;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("webapi")
 public class JAXRSConfiguration extends Application {
 
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(ExampleDataController.class);
+        classes.add(UserController.class);
+        return classes;
+    }
 }
